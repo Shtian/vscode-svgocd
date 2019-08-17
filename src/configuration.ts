0faca12d1efad9e62973145e091beba1fd953e31
Stian Haga <hagastian@gmail.com>
2019-08-17 14:19:04 +0200

    Feat/add js2svg options (#6)
    * chore: add js2svg options
    
    * feat: read js2svg configuration
    
    * chore: ignore vscode test folder
    
    * ci: skip running tests for now

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,9 @@
-import { Options } from 'svgo';
+import { Options, Js2SvgOptions } from 'svgo';
 import { workspace } from 'vscode';
 
-export const readConfiguration = (section: string, value: string): any => {
+const readExtensionConfiguration = <T>(section: string, value: string): T | null => {
     const configSection = workspace.getConfiguration(section);
-    const configValue = configSection.get<any>(value);
+    const configValue = configSection.get<T>(value);
 
     if (typeof configValue === 'undefined') {
         return null;
@@ -12,18 +12,30 @@ export const readConfiguration = (section: string, value: string): any => {
     return configValue;
 };
 
-export const getSVGOPluginsConfig = (): Options => {
-    const pluginsValues = readConfiguration('svgocd', 'plugins');
+const getSVGOJS2SVGConfig = (): Js2SvgOptions | undefined => {
+    const js2svgConfig: Js2SvgOptions | undefined = readExtensionConfiguration<any>('svgocd', 'js2svg');
+    return js2svgConfig;
+};
+
+const getSVGOPluginsConfig = (): any[] => {
+    const pluginsValues = readExtensionConfiguration<any>('svgocd', 'plugins');
     // TODO: Read .svgo.y(a)ml with js.yaml
     const plugins: any[] = [];
 
     if (!pluginsValues) {
-        return { plugins };
+        return plugins;
     }
 
     Object.keys(pluginsValues).map((c) => {
         plugins.push({ [c]: pluginsValues[c] });
     });
 
-    return { plugins };
+    return plugins;
+};
+
+export const getSVGOConfig = (): Options => {
+    const plugins = getSVGOPluginsConfig();
+    const js2svg = getSVGOJS2SVGConfig();
+
+    return { plugins, js2svg };
 };
