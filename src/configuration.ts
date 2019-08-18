f16137716a3552938be3f07d1a563eba492cfb06
Stian Haga <hagastian@gmail.com>
2019-08-18 14:36:17 +0200

    Fix/use eslint prettier (#16)
    * chore: replace tslint with eslint
    
    * chore: format ts files to typescript-eslint:recommended and prettier formatting
    
    * chore: remove tests for now
    
    * chore: remove config files from vscodeignore
    
    * ci: run linting on ci pipeline
    
    * chore: add linting npm script
    
    * docs: add readme badges
    
    * chore: remove unused packages

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,59 +4,59 @@ import * as yaml from 'js-yaml';
 import * as deepmerge from 'deepmerge';
 
 const readExtensionConfiguration = <T>(section: string, value: string): T | null => {
-    const configSection = workspace.getConfiguration(section);
-    const configValue = configSection.get<T>(value);
+  const configSection = workspace.getConfiguration(section);
+  const configValue = configSection.get<T>(value);
 
-    if (typeof configValue === 'undefined') {
-        return null;
-    }
+  if (typeof configValue === 'undefined') {
+    return null;
+  }
 
-    return configValue;
+  return configValue;
 };
 
 const readSVGOYamlConfiguration = async (): Promise<Options | null> => {
-    const [ rootFolder = null ] = workspace.workspaceFolders || [];
-    if (!rootFolder) {
-        return null;
-    }
+  const [rootFolder = null] = workspace.workspaceFolders || [];
+  if (!rootFolder) {
+    return null;
+  }
 
-    const [ configFilePath ] = await workspace.findFiles(new RelativePattern(rootFolder, '.svgo.{yml,yaml}'));
-    if (!configFilePath) {
-        return null;
-    }
+  const [configFilePath] = await workspace.findFiles(new RelativePattern(rootFolder, '.svgo.{yml,yaml}'));
+  if (!configFilePath) {
+    return null;
+  }
 
-    const configFile = await workspace.openTextDocument(configFilePath);
-    return yaml.safeLoad(configFile.getText());
+  const configFile = await workspace.openTextDocument(configFilePath);
+  return yaml.safeLoad(configFile.getText());
 };
 
 const getSVGOJS2SVGConfig = (): Js2SvgOptions | undefined => {
-    const js2svgConfig: Js2SvgOptions | undefined = readExtensionConfiguration<any>('svgocd', 'js2svg');
-    return js2svgConfig;
+  const js2svgConfig: Js2SvgOptions | undefined = readExtensionConfiguration<any>('svgocd', 'js2svg');
+  return js2svgConfig;
 };
 
 const getSVGOPluginsConfig = (): any[] => {
-    const pluginsValues = readExtensionConfiguration<any>('svgocd', 'plugins');
-    // TODO: Read .svgo.y(a)ml with js.yaml
-    const plugins: any[] = [];
+  const pluginsValues = readExtensionConfiguration<any>('svgocd', 'plugins');
+  // TODO: Read .svgo.y(a)ml with js.yaml
+  const plugins: any[] = [];
 
-    if (!pluginsValues) {
-        return plugins;
-    }
-
-    Object.keys(pluginsValues).map((c) => {
-        plugins.push({ [c]: pluginsValues[c] });
-    });
-
+  if (!pluginsValues) {
     return plugins;
+  }
+
+  Object.keys(pluginsValues).map(c => {
+    plugins.push({ [c]: pluginsValues[c] });
+  });
+
+  return plugins;
 };
 
 export const getSVGOConfig = async (): Promise<Options> => {
-    const plugins = getSVGOPluginsConfig();
-    const js2svg = getSVGOJS2SVGConfig();
-    const svgoYmlConfig = await readSVGOYamlConfiguration();
-    if (!svgoYmlConfig) {
-        return { plugins, js2svg };
-    }
+  const plugins = getSVGOPluginsConfig();
+  const js2svg = getSVGOJS2SVGConfig();
+  const svgoYmlConfig = await readSVGOYamlConfiguration();
+  if (!svgoYmlConfig) {
+    return { plugins, js2svg };
+  }
 
-    return deepmerge({ plugins, js2svg }, svgoYmlConfig);
+  return deepmerge({ plugins, js2svg }, svgoYmlConfig);
 };
--- src/optimizeSVG.ts
+++ src/optimizeSVG.ts
@@ -4,42 +4,44 @@ import { getSVGOConfig } from './configuration';
 import { readCurrentFileContent, replaceDocument, readCurrentSelection, replaceSelection } from './utils';
 
 export default class OptimizeSVGO {
-    private svgoConfiguration: SVGO.Options;
+  private svgoConfiguration: SVGO.Options;
 
-    constructor() {
-        this.svgoConfiguration = { plugins: [] };
-        this.readConfiguration();
+  constructor() {
+    this.svgoConfiguration = { plugins: [] };
+    this.readConfiguration();
+  }
+
+  public async readConfiguration(): Promise<void> {
+    this.svgoConfiguration = await getSVGOConfig();
+  }
+
+  public async optimizeSVG(): Promise<boolean> {
+    const selection = readCurrentSelection();
+    const text = readCurrentFileContent();
+    const svgToOptimize = selection || text;
+
+    if (!svgToOptimize) {
+      window.showErrorMessage(
+        'This extension can only be run with an SVG file open or having an SVG selected in the current document.'
+      );
+      return false;
     }
 
-    public async readConfiguration() {
-        this.svgoConfiguration = await getSVGOConfig();
+    try {
+      const svgo = new SVGO(this.svgoConfiguration);
+      const optimizedSVG = await svgo.optimize(svgToOptimize);
+
+      if (selection) {
+        replaceSelection(optimizedSVG.data);
+      } else {
+        replaceDocument(optimizedSVG.data);
+      }
+
+      window.showInformationMessage('SVG Optimized ✨');
+      return true;
+    } catch (error) {
+      window.showErrorMessage(`Error during SVG Optimization: ${error}`);
+      return false;
     }
-
-    public async optimizeSVG() {
-        const selection = readCurrentSelection();
-        const text = readCurrentFileContent();
-        const svgToOptimize = selection || text;
-
-        if (!svgToOptimize) {
-            window.showErrorMessage(
-                'This extension can only be run with an SVG file open or having an SVG selected in the current document.'
-            );
-            return;
-        }
-
-        try {
-            const svgo = new SVGO(this.svgoConfiguration);
-            const optimizedSVG = await svgo.optimize(svgToOptimize);
-
-            if (selection) {
-                replaceSelection(optimizedSVG.data);
-            } else {
-                replaceDocument(optimizedSVG.data);
-            }
-
-            window.showInformationMessage('SVG Optimized ✨');
-        } catch (error) {
-            window.showErrorMessage(`Error during SVG Optimization: ${error}`);
-        }
-    }
+  }
 }
